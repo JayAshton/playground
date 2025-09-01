360da9f9a73be4a82b3c2287adf7f37596ea4554
Jay Ashton <jay.ashton@justice.gov.uk>
2025-09-01 14:53:34 +0100

    Using postgres to for products

--- microservices/products-service/src/main/kotlin/com/example/products/ProductService.kt
+++ microservices/products-service/src/main/kotlin/com/example/products/ProductService.kt
@@ -1,5 +1,8 @@
 package com.example.products
 
+import org.springframework.stereotype.Service
+
+@Service
 class ProductService(private val repository: ProductRepository) {
     fun findAll(): List<Product> = repository.findAll()
 }
